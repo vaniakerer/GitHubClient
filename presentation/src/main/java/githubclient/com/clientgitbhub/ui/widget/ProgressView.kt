230af1dee76f69784813b81ea633c05830af0e64
Ivan <vaniakerer8@gmail.com>
2018-08-31 00:45:13 +0300

    sketches for custom progress

--- presentation/src/main/java/githubclient/com/clientgitbhub/ui/widget/ProgressView.kt
+++ presentation/src/main/java/githubclient/com/clientgitbhub/ui/widget/ProgressView.kt
@@ -1,11 +1,19 @@
 package githubclient.com.clientgitbhub.ui.widget
 
+import android.animation.Keyframe
+import android.animation.PropertyValuesHolder
+import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.os.Handler
 import android.util.AttributeSet
+import android.util.Property
 import android.view.View
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.Animation
+import android.view.animation.BounceInterpolator
 
 /**
  * @author Ivan Kerer
@@ -15,18 +23,53 @@ class ProgressView @JvmOverloads constructor(
         context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
 
-    private val firstCirclePaint: Paint = Paint()
+    private val circlePaint: Paint = Paint()
 
-    private var firstX: Int = 0
+    private var radius = 0f
+    private val difference = 30
+    private val circlesCount = 3
+
 
     init {
-        firstCirclePaint.color = Color.CYAN
-
-        firstX = bottom - (top - bottom) / 2
+        circlePaint.color = Color.parseColor("#2D000000")
+        Handler().postDelayed(
+                {
+                    startAnimation()
+                },
+                2000
+        )
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        canvas?.drawCircle(left.toFloat(), firstX.toFloat(), 100f, firstCirclePaint)
+        canvas?.drawColor(Color.TRANSPARENT)
+        val x = (width / 2).toFloat()
+        val y = (height / 2).toFloat()
+
+        for (it in 0..circlesCount)
+            canvas?.drawCircle(x, y, radius - difference * it, circlePaint)
+    }
+
+    private fun startAnimation() {
+        val keyFrame1 = Keyframe.ofFloat(0f, 0f)
+        val keyFrame2 = Keyframe.ofFloat(.5f, (width / 2).toFloat())
+        val keyFrame3 = Keyframe.ofFloat(1f, 0f)
+
+        val valueAnimator = ValueAnimator
+                .ofPropertyValuesHolder(
+                        PropertyValuesHolder
+                                .ofKeyframe("af", keyFrame1, keyFrame2, keyFrame3)
+                )
+
+        valueAnimator.duration = 3000
+        valueAnimator.addUpdateListener {
+            radius = it.animatedValue as Float
+            invalidate()
+        }
+
+        valueAnimator.interpolator = AccelerateInterpolator()
+
+        valueAnimator.repeatCount = Animation.INFINITE
+        valueAnimator.start()
     }
 }
